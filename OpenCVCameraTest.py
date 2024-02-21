70eb7d9800c4b285c151d64bf3139c25a4d17a48
KavinaashJesurajah <134456223+KavinaashJesurajah@users.noreply.github.com>
2024-02-21 14:15:04 -0500

    Add files via upload
    Code that uses OpenCV USB Webcam

--- OpenCVCameraTest.py
+++ OpenCVCameraTest.py
@@ -6,13 +6,17 @@ folder_path = 'images'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
+# Initialize the USB webcam (change the index if needed)
+cap = cv2.VideoCapture(1)  # Change the index to 0, 1, 2, etc. depending on your setup
 
 # Check if the webcam is opened successfully
 if not cap.isOpened():
     print("Error: Could not open webcam")
     exit()
+
+# Set the resolution to 1920x1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 # Capture a frame from the webcam
 ret, frame = cap.read()
@@ -34,3 +38,4 @@ print("Image saved successfully at", image_path)
 # Release the webcam and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
+
